eb9cfffb1245f3cb0f5cde3893397e7a9a8a6b8b
Albjon Hilaj <143876464+AlbjonHi@users.noreply.github.com>
2023-09-21 21:54:34 +0200

    add: Empty isFileChanged method

--- src/main/java/com/amarildo/zettelkastenanalyzer/service/NoteService.java
+++ src/main/java/com/amarildo/zettelkastenanalyzer/service/NoteService.java
@@ -1,11 +1,14 @@
 package com.amarildo.zettelkastenanalyzer.service;
 
+import java.io.File;
+
 public class NoteService {
-
-    // TODO (21/09/2023): method for analyzing whether the file has changed in some way
-
+    public boolean isFileChanged(File f) {
+        // TODO (21/09/2023): method for analyzing whether the file has changed in some way
+        return false;
+    }
+    
     // TODO (21/09/2023): method for parsing a folder hierarchy recursively file by file
-
+    
     // TODO (21/09/2023): method to extract all the words contained in a file
-
 }
