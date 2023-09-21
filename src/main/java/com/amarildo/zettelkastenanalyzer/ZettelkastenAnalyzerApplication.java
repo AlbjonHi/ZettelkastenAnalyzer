cc403cd81b275251bc0ab50f0603942b1b912bcc
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2023-09-21 20:20:35 +0200

    add: Spring Boot base project

---
+++ src/main/java/com/amarildo/zettelkastenanalyzer/ZettelkastenAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.amarildo.zettelkastenanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZettelkastenAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZettelkastenAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amarildo/zettelkastenanalyzer/ZettelkastenAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarildo.zettelkastenanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZettelkastenAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
