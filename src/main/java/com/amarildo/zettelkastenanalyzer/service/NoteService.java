d36a73dc943eb8b95dfccb348ea629a992837064
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2023-09-22 21:47:26 +0200

    add: Todo

--- src/main/java/com/amarildo/zettelkastenanalyzer/service/NoteService.java
+++ src/main/java/com/amarildo/zettelkastenanalyzer/service/NoteService.java
@@ -11,4 +11,6 @@ public class NoteService {
     // TODO (21/09/2023): method for parsing a folder hierarchy recursively file by file
     
     // TODO (21/09/2023): method to extract all the words contained in a file
+
+    // TODO (22/09/2023): creating a story about the progress of the zettelkasten. i want to display graphs that show the trend over time based on the chosen filters
 }
