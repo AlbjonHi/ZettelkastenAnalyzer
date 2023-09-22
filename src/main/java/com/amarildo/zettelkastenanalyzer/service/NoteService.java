3834b90880ec3564cfcc62fe48a8054640135a57
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2023-09-22 22:11:53 +0200

    update: Todo

--- src/main/java/com/amarildo/zettelkastenanalyzer/service/NoteService.java
+++ src/main/java/com/amarildo/zettelkastenanalyzer/service/NoteService.java
@@ -5,12 +5,13 @@ import java.io.File;
 public class NoteService {
     public boolean isFileChanged(File f) {
         // TODO (21/09/2023): method for analyzing whether the file has changed in some way
+        // TODO (22/09/2023): 
         return false;
     }
     
     // TODO (21/09/2023): method for parsing a folder hierarchy recursively file by file
     
-    // TODO (21/09/2023): method to extract all the words contained in a file
+    // TODO (22/09/2023): method to extract all the words contained in a file. Note that in markdown, inline code starts and ends with the "`" character. this character should be ignored in word searches
 
     // TODO (22/09/2023): creating a story about the progress of the zettelkasten. i want to display graphs that show the trend over time based on the chosen filters
 
