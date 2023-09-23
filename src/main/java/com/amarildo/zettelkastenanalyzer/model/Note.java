2b726f0486df8942e4c6b692839df263a6fed405
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2023-09-23 13:56:26 +0200

    refactor: Note variables

--- src/main/java/com/amarildo/zettelkastenanalyzer/model/Note.java
+++ src/main/java/com/amarildo/zettelkastenanalyzer/model/Note.java
@@ -13,5 +13,9 @@ import lombok.NoArgsConstructor;
 public class Note {
 
     @Id
+    private String title;
+    private String text;
+    private String category;
     private Priority priority;
+    private boolean notesAddedToAnki;
 }
